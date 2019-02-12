0997f7d437bae96a04ed8ac1e28b17c0e17d1332
Florian Zettelmeyer <florianz@gmail.com>
2019-02-12 17:04:14 -0600

    1. Rework of gainsplot to handle the problem

--- R/gainsplot.R
+++ R/gainsplot.R
@@ -3,37 +3,45 @@
 #' This function allows you to compare model performace by comparing the gains curves of models.
 #' @param label (required)
 #' @param score1, score 2, ... (at least 1 required)
-#' @param bin (defaults to 10) 
 #' @keywords gains, auc
 #' @export
 #' @examples
-#' gainsplot(label.var = bbb$buyer, logit1$fitted.values, rf$fitted.values, nn$fitted.values)
+#' gainsplot(logit1$fitted.values, rf$fitted.values, nn$fitted.values, label.var = bbb$buyer)
 
-gainsplot <- function(label.var,..., bin = 10) {
-  pred.vars <- tibble(...)
-  pred.vars
+gainsplot2 <- function(label.var,...) {
+  arglist <- list(...)
+  for (i in 1:length(arglist)){
+    arglist[[i]] <- as.vector(arglist[[i]])
+  }
+  
+  pred.vars <- as.tibble(do.call(cbind, arglist))  
+  
+  for (i in 3:length(match.call())){
+    names(pred.vars)[i-2] <- paste(match.call()[[i]])
+  }
+  
   gains.data.build <- NULL
   auc.build <- NULL
   for (i in seq_along(pred.vars)) {
     pred.var <- pred.vars[[i]]
     pred <- ROCR::prediction(pred.var,factor(label.var))
     gain <- ROCR::performance(pred, "tpr", "rpp")
-    gains.data <- tibble(Model = colnames(pred.vars)[i], 
+    gains.data <- tibble(Model = colnames(pred.vars)[i],
                          Percent.buyers=as.numeric(unlist(gain@y.values)),
                          Percent.customers=as.numeric(unlist(gain@x.values))) %>%
-      mutate(Percent.buyers=Percent.buyers*100, 
+      mutate(Percent.buyers=Percent.buyers*100,
              Percent.customers=Percent.customers*100)
-      auc <- bind_cols(model = colnames(pred.vars)[i], 
-                       auc =  round(unlist(ROCR::performance(pred, measure = "auc")@y.values),3))
+    auc <- bind_cols(model = colnames(pred.vars)[i],
+                     auc =  round(unlist(ROCR::performance(pred, measure = "auc")@y.values),3))
     auc.build <- bind_rows(auc.build, auc)
     gains.data.build <- bind_rows(gains.data.build, gains.data)
   }
   no.model.data <- tbl_df(data.frame(Percent.buyers=c(0,100),
                                      Percent.customers=c(0,100)))
-  print(ggplot() + 
-          geom_line(data=gains.data.build, 
+  print(ggplot() +
+          geom_line(data=gains.data.build,
                     aes(Percent.customers,Percent.buyers,color = Model)) +
-          geom_line(data=no.model.data, 
+          geom_line(data=no.model.data,
                     aes(Percent.customers,Percent.buyers), linetype=3) +
           labs(x="Percent Customers",
                y="Percent Buyers"))
