bc059b3c4729d0a12610b7ee50c82fb5de361397
Florian Zettelmeyer <florianz@gmail.com>
2020-05-19 12:33:59 -0500

    1. Introduces ss.propotion and ss.2proportion to estimate sample size

--- R/expdesign.R
+++ R/expdesign.R
@@ -26,3 +26,46 @@ get.estimable.coefficients <- function(full.design, frac.design){
 	return(list( Estimable=names(coef.frac)[-1],
 				 Inestimable=setdiff(names(coef.full), names(coef.frac)) ))
 }
+
+#' A function to determine the sample size required to estimate a proportion given a desired confidence level and interval half width
+#' @param halfwidth (required)
+#' @param confidence (optional with default of .95)
+#' @param proportion (optional with default of .5)
+#' @keywords sample size calcuation
+#' @export
+#' @examples
+#' ss.proportion(0.03)
+#' ss.proportion(0.03, confidence=0.99)
+#' ss.proportion(0.03, confidence=0.99, proportion=.1)
+
+ss.proportion <- function(halfwidth, confidence=0.95, proportion=0.5){
+  if(halfwidth <= 0){ return("Halfwidth must be greater than 0.") }
+  if(confidence <= 0 | confidence >= 1){ return("Confidence must be between 0 and 1.") }
+  if(proportion <= 0 | proportion >= 1){ return("Proportion must be between 0 and 1.") }
+  z <- abs(qnorm((1 - confidence) / 2))
+  pq <- proportion * (1-proportion)
+  return(ceiling(z^2 * pq / halfwidth^2))
+}
+
+#' A function to determine the sample size required to estimate the difference of two proportions given a desired confidence level and interval half width
+#' @param halfwidth (required)
+#' @param confidence (optional with default of .95)
+#' @param proportion1 (optional with default of .5)
+#' @param proportion2 (optional with default of .5)
+#' @keywords sample size calcuation
+#' @export
+#' @examples
+#' ss.2proportion(0.03)
+#' ss.2proportion(0.03, confidence=0.99)
+#' ss.2proportion(0.03, confidence=0.99, proportion1=.1, proportion1=.2)
+
+ss.2proportion <- function(halfwidth, confidence=0.95, proportion1=0.5, proportion2=0.5){
+  if(halfwidth <= 0){ return("Halfwidth must be greater than 0.") }
+  if(confidence <= 0 | confidence >= 1){ return("Confidence must be between 0 and 1.") }
+  if(proportion1 <= 0 | proportion1 >= 1){ return("Proportions must be between 0 and 1.") }
+  if(proportion2 <= 0 | proportion2 >= 1){ return("Proportions must be between 0 and 1.") }
+  z <- abs(qnorm((1 - confidence) / 2))
+  pq1 <- proportion1 * (1-proportion1)
+  pq2 <- proportion2 * (1-proportion2)
+  return(ceiling(z^2 * (pq1 + pq2) / halfwidth^2))
+}
